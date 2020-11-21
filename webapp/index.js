bc55e5754494b72867d619df5c42b56a8cc3ffec
Prasetyo W <prasetyowls12@yahoo.com>
2020-11-21 16:00:12 +0700

    Added Webapp

---
+++ webapp/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const socket = require("socket.io");
+const spawn = require('child_process').spawn;
+// const spawn = require('child_process').spawn;
+// const py    = spawn('python', ['main.py']);
+// var dataString = '';
+
+// App setup
+const PORT = 3000;
+const app = express();
+const server = app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
+});
+
+var req_count = 0;
+
+// Static files
+app.use(express.static("public"));
+
+// Socket setup
+const io = socket(server);
+
+io.on("connection", function (socket) {
+  console.log("Made socket connection");
+
+
+  socket.on("inputmsg", function (data) {
+    // Olah data
+    var py    = spawn('python', ['main.py', data]);
+
+    py.stdout.on('data', function(data){
+      dataString = data.toString();
+    });
+
+    py.stdout.on('end', function(){
+      req_count += 1;
+      console.log("Req Count:", req_count);
+      console.log("Input:", data);
+      console.log("Output:", dataString);
+      
+      io.emit("reply", dataString);
+    });
+
+  });
+
+});
---
+++ webapp/public/main.js
@@ -0,0 +1,24 @@
+
+const socket = io();
+
+const replymsg = document.querySelector(".replymsg");
+const inputmsg = document.querySelector(".inputmsg_text");
+const inputmsgForm = document.querySelector(".inputmsg");
+
+function copyFunc() {
+  navigator.clipboard.writeText(replymsg.value);
+  alert("Copied!");
+}
+
+inputmsgForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!inputmsg.value) {
+    return;
+  }
+  socket.emit("inputmsg", inputmsg.value);
+});
+
+socket.on("reply", function (data) {
+  console.log(data);
+  replymsg.value = data;
+});
