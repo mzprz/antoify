3d96cdc7e672d1ccf8df65a6514e6813e7c3c521
Prasetyo W <prasetyowls12@yahoo.com>
2020-11-21 18:03:38 +0700

    Added socket id

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const socket = require("socket.io");
 const spawn = require('child_process').spawn;
-// const spawn = require('child_process').spawn;
-// const py    = spawn('python', ['main.py']);
-// var dataString = '';
 
 // App setup
 const PORT = process.env.PORT || 3000;
@@ -13,19 +10,19 @@ const server = app.listen(PORT, function () {
   console.log(`http://localhost:${PORT}`);
 });
 
+// Metrics & Output
 var req_count = 0;
 var dataString = '';
 
 // Static files
 app.use(express.static("public"));
 
+
 // Socket setup
 const io = socket(server);
 
-io.on("connection", function (socket) {
-  console.log("Made socket connection");
-
-
+io.on("connection", function(socket) {
+  // console.log("Made socket connection");
   socket.on("inputmsg", function (data) {
     // Olah data
     var py    = spawn('python', ['main.py', data]);
@@ -37,12 +34,12 @@ io.on("connection", function (socket) {
     py.stdout.on('end', function(){
       req_count += 1;
       console.log("Req Count:", req_count);
+      console.log("Socket ID: " + socket.id);
       console.log("Input:", data);
       console.log("Output:", dataString);
 
-      io.emit("reply", dataString);
+      io.to(socket.id).emit("reply", dataString);
     });
 
   });
-
 });
